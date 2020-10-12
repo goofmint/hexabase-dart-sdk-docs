60b390a71e1197d74ec7d74439315dcac89304f4
jeane paul soliva <jeanepaul@gmail.com>
2020-10-12 23:08:39 +0900

    update url

--- siteConfig.js
+++ siteConfig.js
@@ -1,5 +1,5 @@
 const siteConfig = {
-    url: 'https://b-eee.github.io', // Your website URL
+    url: 'https://b-eee.github.io/hexabase-sdk-docs/', // Your website URL
     baseUrl: '/hexabase-sdk-docs/',
     projectName: 'hexabase-sdk-docs',
     organizationName: 'b-eee'
