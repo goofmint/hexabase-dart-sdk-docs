2aa5aada8fc1ebec89a92fbc7471089afd647103
Nguyenpd-lts <nguyenpd@lotus-ts.com>
2022-04-20 15:39:22 +0700

    update search

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -70,24 +70,7 @@ module.exports = {
       ],
       copyright: `Copyright © ${new Date().getFullYear()} Hexabase-SDK. Built with Docusaurus.`,
     },
-    algolia: {
-      // The application ID provided by Algolia
-      appId: 'TT5KY47HTN',
-
-      // Public API key: it is safe to commit it
-      apiKey: '86c500a399a786aaacd4d2b28f177a1e',
-
-      indexName: 'dev_hxb',
-
-
-      // Optional: path for search page that enabled by default (`false` to disable it)
-      searchPagePath: 'search',
-      contextualSearch: true,
-      // searchParameters: {
-      //   facetFilters: ['language:en', ['filter1', 'filter2'], 'filter3'],
-      // },
-      //... other Algolia params
-    },
+    
   },
   presets: [
     [
@@ -111,5 +94,41 @@ module.exports = {
       },
     ],
   ],
-  // themes: ['@docusaurus/theme-search-algolia'],
+  plugins: [
+    [require.resolve('@cmfcmf/docusaurus-search-local'), {
+        // whether to index docs pages
+        indexDocs: true,
+        // must start with "/" and correspond to the routeBasePath configured for the docs plugin
+        // use "/" if you use docs-only-mode
+        // (see https://v2.docusaurus.io/docs/2.0.0-alpha.70/docs-introduction#docs-only-mode)
+        docsRouteBasePath: '/docs',
+
+        // Whether to also index the titles of the parent categories in the sidebar of a doc page.
+        // 0 disables this feature.
+        // 1 indexes the direct parent category in the sidebar of a doc page
+        // 2 indexes up to two nested parent categories of a doc page
+        // 3...
+        //
+        // Do _not_ use Infinity, the value must be a JSON-serializable integer.
+        indexDocSidebarParentCategories: 0,
+
+        // whether to index static pages
+        // /404.html is never indexed
+        indexPages: false,
+
+        // language of your documentation, see next section
+        language: ["en", "ja"],
+
+        // lunr.js-specific settings
+        lunr: {
+            // When indexing your documents, their content is split into "tokens".
+            // Text entered into the search box is also tokenized.
+            // This setting configures the separator used to determine where to split the text into tokens.
+            // By default, it splits the text at whitespace and dashes.
+            //
+            // Note: Does not work for "ja" and "th" languages, since these use a different tokenizer.
+            tokenizerSeparator: /[\s\-\u{3000}-\u{301C}\u{3041}-\u{3093}\u{309B}-\u{309E}]+/gu
+        }
+    }]
+  ],
 };
