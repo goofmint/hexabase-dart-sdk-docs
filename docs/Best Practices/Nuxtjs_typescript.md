62d7539afd02d172c9d261bb4731e458d6d24b44
Nguyen Duy Ban <BanND@ltsgroup.tech>
2022-10-19 17:27:25 +0700

    update nuxt-web example create workspace, create project, create delete item

--- docs/Best Practices/Nuxtjs_typescript.md
+++ docs/Best Practices/Nuxtjs_typescript.md
@@ -115,11 +115,15 @@ export default defineNuxtConfig({
 });
 ```
 
-### Add services
+### Create services
 
-`user.service.ts` to handle login / logout services
+Firstly, create auth services to handle `login`, `logout` or `register`
+
+For example, add `login` function to user.service
 
 ```tsx
+//user.service.ts
+
 import { createClient } from "@hexabase/hexabase-js";
 import { useRoute, useRouter } from "nuxt/app";
 
@@ -151,44 +155,192 @@ async function login(baseUrl: string, email: string, password: string) {
   }
   return token;
 }
+
+//add other service to handle authentication
+//Auth guard should be created to handle auth-related redirects or further bussiness logics
 ```
 
 ![An image from the static](/img/screenshot-login.png)
 
-For other services, such as `workspace.service.ts`
+## Fetching data
+
+After login, let's get all available service.
 
 ```tsx
+// workspace.service.ts
+
 import { createClient, HexabaseClient } from "@hexabase/hexabase-js";
 import { useRuntimeConfig } from "nuxt/app";
 export const workspaceService = {
   getWorkspaces,
 };
 //create client variant first
+//other functions in this service should reuse this
 async function initHxbClient() {
-  // get token from localstorage
-  // get base url from runtimeConfig that described in `nuxt.config.ts`
   const token = JSON.parse(localStorage.getItem("user")!).token;
-  const hexabase =
-    token &&
-    (await createClient({ url: useRuntimeConfig().public.baseUrl, token }));
-  return hexabase;
+  const url = useRuntimeConfig().public.baseUrl;
+  if (token && url) {
+    const hexabase = await createClient({ url, token });
+    return hexabase;
+  }
 }
 
-// get all workspaces
 async function getWorkspaces() {
   const hexabase = await initHxbClient();
-  //call method of hexabase client
   const { workspaces, error } = await hexabase.workspaces.get();
   return workspaces;
 }
 ```
 
-##### workspace
+Once get workspaces list, we can render the _`<Select />`_ to choose a specific workspace to show its detail including projects and datastores. As example bellow, workspace with name _`Nguyen workspace`_ is selected to be displayed.
+
+Let's pass the `current_workspace_id` to `getProjectsAndDatastores` api to get all projects and datastore of that workspace:
+
+```tsx
+//application.service.ts
+
+async function getAppAndDs(id: string) {
+  const hexabase = await initHxbClient();
+  const { appAndDs, error } =
+    await hexabase.applications.getProjectsAndDatastores(id);
+  return appAndDs;
+}
+```
 
 ![An image from the static](/img/screenshot-workspace.png)
 
-#### then you can create site to display datastores of each project
+You can create a new _`workspace`_
 
-##### datastores
+```tsx
+//workspace.service.ts
 
-![An image from the static](/img/screenshot-datastore.png)
+async function createWorkspace(name: string) {
+  const createWsInput = {
+    name,
+  };
+  const hexabase = await initHxbClient();
+  const { w_id, error } = await hexabase.workspaces.create(createWsInput);
+  return w_id;
+}
+```
+
+Or create a new _`application`_ in current workspace
+
+### _NOTE: `application` and `project` is equivalent in terms of meaning_
+
+```tsx
+//application.service.ts
+
+import { CreateProjectPl } from "@hexabase/hexabase-js/src/lib/types/application";
+
+async function createApp(createProjectParams: CreateProjectPl) {
+  const hexabase = await initHxbClient();
+  const { app, error } = await hexabase.applications.create(
+    createProjectParams
+  );
+  return app?.project_id;
+}
+```
+
+![An image from the static](/img/screenshot-createApp.png)
+
+You can use a template, it is optional. Then you can create site to display detail information of datastores
+
+```tsx
+//item.service.ts
+
+//get items of a datastore
+async function getItems(
+  projectId: string,
+  datastoreId: string,
+  getItemsParameters: GetItemsPl
+) {
+  const hexabase = await initHxbClient();
+  const { dsItems, error } = await hexabase.items.get(
+    getItemsParameters,
+    datastoreId,
+    projectId
+  );
+  return dsItems;
+}
+
+//get item detail
+async function getItemDetail(
+  datastoreId: string,
+  itemId: string,
+  projectId: string,
+  itemDetailParams: GetItemDetailPl
+) {
+  const hexabase = await initHxbClient();
+  const { itemDetails, error } = await hexabase.items.getItemDetail(
+    datastoreId,
+    itemId,
+    projectId,
+    itemDetailParams
+  );
+  return itemDetails;
+}
+```
+
+After get items from a datastore by call `getItems` function, you should call `getFields` as well, to get should-be-displayed fields according to `fields setting`.
+
+```tsx
+//datastore.service.ts
+
+async function getFields(datastoreId: string, projectId: string) {
+  const hexabase = await initHxbClient();
+  const { dsFields, error } = await hexabase.datastores.getFields(
+    datastoreId,
+    projectId
+  );
+  return dsFields;
+}
+```
+
+_`/workspace/workspace_id/project/project_id/datastore/datastore_id`_
+![An image from the static](/img/screenshot-item.png)
+
+Click to any item to view the detail. The initial item detail automatically taken from the first elemtent of the table
+
+![An image from the static](/img/screenshot-item-viewDetail.png)
+
+You can delete an item
+
+```tsx
+//item.service.ts
+
+async function deleteItem(
+  projectId: string,
+  datastoreId: string,
+  itemId: string,
+  deleteItemReq: DeleteItemReq
+) {
+  const hexabase = await initHxbClient();
+  const { data, error } = await hexabase.items.delete(
+    projectId,
+    datastoreId,
+    itemId,
+    deleteItemReq
+  );
+  return data;
+}
+```
+
+or add an item
+![An image from the static](/img/screenshot-createItem.png)
+
+```tsx
+async function createItem(
+  projectId: string,
+  datastoreId: string,
+  newItemPl: CreateNewItemPl
+) {
+  const hexabase = await initHxbClient();
+  const { itemNew, error } = await hexabase.items.create(
+    projectId,
+    datastoreId,
+    newItemPl
+  );
+  return itemNew;
+}
+```
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,107 +1,119 @@
 module.exports = {
-  title: 'Hexabase SDK',
-  tagline: 'Typescript SDK for Hexabase API',
-  url: 'https://b-eee.github.io',
-  baseUrl: '/',
-  onBrokenLinks: 'ignore',
-  favicon: 'img/favicon.ico',
-  organizationName: 'b-eee', // Usually your GitHub org/user name.
-  projectName: 'hexabase-sdk-docs', // Usually your repo name.
+  title: "Hexabase SDK",
+  tagline: "Typescript SDK for Hexabase API",
+  url: "https://b-eee.github.io",
+  baseUrl: "/",
+  onBrokenLinks: "ignore",
+  favicon: "img/favicon.ico",
+  organizationName: "b-eee", // Usually your GitHub org/user name.
+  projectName: "hexabase-sdk-docs", // Usually your repo name.
   themeConfig: {
+    zoom: {
+      selector: ".markdown :not(em) > img",
+      config: {
+        // options you can specify via https://github.com/francoischalifour/medium-zoom#usage
+        background: {
+          light: "rgb(255, 255, 255)",
+          dark: "rgb(50, 50, 50)",
+        },
+      },
+    },
     navbar: {
-      title: 'Hexabase SDK',
+      title: "Hexabase SDK",
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        alt: "My Site Logo",
+        src: "img/logo.svg",
       },
       items: [
         {
-          to: 'docs/Hexabase/Installation',
-          activeBasePath: 'docs',
-          label: 'Docs',
-          position: 'left',
+          to: "docs/Hexabase/Installation",
+          activeBasePath: "docs",
+          label: "Docs",
+          position: "left",
         },
-        {to: 'blog', label: 'Blog', position: 'left'},
+        { to: "blog", label: "Blog", position: "left" },
         {
-          href: 'https://github.com/b-eee/hexabase-sdk-docs',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/b-eee/hexabase-sdk-docs",
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'API Docs',
+          title: "API Docs",
           items: [
             {
-              label: 'Redoc',
-              to: 'https://az-lk.hexabase.com/redoc/index.html?url=/swagger/v1/swagger.json',
+              label: "Redoc",
+              to: "https://az-lk.hexabase.com/redoc/index.html?url=/swagger/v1/swagger.json",
             },
             {
-              label: 'Swagger',
-              to: 'https://az-lk.hexabase.com/swagger/index.html',
+              label: "Swagger",
+              to: "https://az-lk.hexabase.com/swagger/index.html",
             },
           ],
         },
         {
-          title: 'Community',
+          title: "Community",
           items: [
             {
-              label: 'Discourse (Community Site)',
-              href: 'https://community.hexabase.com/',
+              label: "Discourse (Community Site)",
+              href: "https://community.hexabase.com/",
             },
           ],
         },
         {
-          title: 'Company',
+          title: "Company",
           items: [
             {
-              label: 'Blog',
-              to: 'blog',
+              label: "Blog",
+              to: "blog",
             },
             {
-              label: 'Company',
-              href: 'https://www.hexabase.com/',
+              label: "Company",
+              href: "https://www.hexabase.com/",
             },
           ],
         },
       ],
       copyright: `Copyright © ${new Date().getFullYear()} Hexabase-SDK. Built with Docusaurus.`,
     },
-    
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/b-eee/hexabase-sdk-docs/edit/master/website/',
+            "https://github.com/b-eee/hexabase-sdk-docs/edit/master/website/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/b-eee/hexabase-sdk-docs/edit/master/website/blog/',
+            "https://github.com/b-eee/hexabase-sdk-docs/edit/master/website/blog/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       },
     ],
   ],
   plugins: [
-    [require.resolve('@cmfcmf/docusaurus-search-local'), {
+    require.resolve("docusaurus-plugin-image-zoom"),
+    [
+      require.resolve("@cmfcmf/docusaurus-search-local"),
+      {
         // whether to index docs pages
         indexDocs: true,
         // must start with "/" and correspond to the routeBasePath configured for the docs plugin
         // use "/" if you use docs-only-mode
         // (see https://v2.docusaurus.io/docs/2.0.0-alpha.70/docs-introduction#docs-only-mode)
-        docsRouteBasePath: '/docs',
+        docsRouteBasePath: "/docs",
 
         // Whether to also index the titles of the parent categories in the sidebar of a doc page.
         // 0 disables this feature.
@@ -121,14 +133,16 @@ module.exports = {
 
         // lunr.js-specific settings
         lunr: {
-            // When indexing your documents, their content is split into "tokens".
-            // Text entered into the search box is also tokenized.
-            // This setting configures the separator used to determine where to split the text into tokens.
-            // By default, it splits the text at whitespace and dashes.
-            //
-            // Note: Does not work for "ja" and "th" languages, since these use a different tokenizer.
-            tokenizerSeparator: /[\s\-\u{3000}-\u{301C}\u{3041}-\u{3093}\u{309B}-\u{309E}]+/gu
-        }
-    }]
+          // When indexing your documents, their content is split into "tokens".
+          // Text entered into the search box is also tokenized.
+          // This setting configures the separator used to determine where to split the text into tokens.
+          // By default, it splits the text at whitespace and dashes.
+          //
+          // Note: Does not work for "ja" and "th" languages, since these use a different tokenizer.
+          tokenizerSeparator:
+            /[\s\-\u{3000}-\u{301C}\u{3041}-\u{3093}\u{309B}-\u{309E}]+/gu,
+        },
+      },
+    ],
   ],
 };
